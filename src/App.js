556da908c0f755e8a4cfa23ae75a4abb769c689e
Aidajy111 <aidasjy4.00@gmail.com>
2024-02-27 01:55:06 +0800

    install Axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./scss/style.scss";
+import axios from "axios";
 
 function App() {
+  const baseUrl = "http://api.valantis.store:40000/";
+
+  const [post, setPost] = useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseUrl).then((r) => {
+      console.log(r);
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header"></header>
